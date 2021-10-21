8116ff8dd0f7d3ee60f3070e0e9838ed2fca6f15
Dion <dionananda77@gmail.com>
2021-10-22 01:47:00 +0700

    get user for profile

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,74 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Avatar, Box, Button, Divider, Flex, Text } from "@chakra-ui/react";
-import { CardProfile, CardUserPosting } from "components";
-import homeRequest from "api/home";
+import React, { useContext } from "react";
+import { CardProfile } from "components";
+import UserContext from "context/user/UserContext";
 
-export default function Profile(props) {
-  const [posting, setPosting] = useState([]);
+export default function Profile() {
+  const { userProfile } = useContext(UserContext);
 
-  const { user } = props;
-
-  const getPostingSelf = async () => {
-    const response = await homeRequest.getPostingSelf();
-    setPosting(response.data);
-  };
-
-  useEffect(() => {
-    getPostingSelf();
-  }, []);
-
-  return (
-    <CardProfile>
-      <Flex mb={10}>
-        <Box>
-          <Avatar width="120px" height="120px" />
-        </Box>
-        <Box ml={20}>
-          <Flex mb={4}>
-            <Text fontSize="26px">Jhon Doe</Text>
-            <Button
-              size="sm"
-              ml={4}
-              mt="0.5"
-              bg="white"
-              borderWidth="1px"
-              borderColor="gray.200"
-              _hover={{ bg: "white" }}
-              _active={{ bg: "white" }}
-              _focus={{ bg: "white", color: "gray.500", borderWidth: "1px" }}
-            >
-              Edit Profile
-            </Button>
-          </Flex>
-          <Flex mb={4}>
-            <Text>
-              <b>0</b> posts
-            </Text>
-            <Text ml={10}>
-              <b>0</b> follower
-            </Text>
-            <Text ml={10}>
-              <b>0</b> following
-            </Text>
-          </Flex>
-          <Text>
-            "Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry."
-          </Text>
-        </Box>
-      </Flex>
-      <Divider />
-      <Box mt={5} mb={5}>
-        <Text>Posting</Text>
-      </Box>
-      {posting.map((item, index) => (
-        <CardUserPosting
-          data={item}
-          user={user}
-          key={index}
-          getPost={getPostingSelf}
-        />
-      ))}
-    </CardProfile>
-  );
+  return <CardProfile maxW="36rem" user={userProfile} />;
 }
